204e9752cbc58047fe45246873efc77476306be3
Tom Chang <s155596@gmail.com>
2020-07-21 21:18:42 +0800

    add header

--- products.py
+++ products.py
@@ -15,3 +15,10 @@ print(products[0][0]) # 代表products中的第一個商品及價格中的商品
 
 for product in products: # 印出清單中的每一個東西(小清單)
 	print(product[0], '的價格是', product[1])
+
+with open('products.csv', 'w', encoding='utf-8') as f: #打開檔案 (檔案已存在也沒關係會覆蓋)
+# with 有自動close的功能
+# encoding(編碼) ='utf-8' (最廣義使用的編碼)
+	f.write('商品,價格\n' ) #在寫入商品價格前有個抬頭
+	for product in products:
+		f.write(product[0] + ',' + product[1] + '\n') # write為寫入的動作
